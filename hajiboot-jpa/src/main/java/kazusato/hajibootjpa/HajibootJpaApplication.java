dd2a283b1ad72ae875bc4751442b32b1eda9c9c1
sato <super80@gmail.com>
2018-04-14 14:11:55 +0900

    Pageable対応完了

--- hajiboot-jpa/src/main/java/kazusato/hajibootjpa/HajibootJpaApplication.java
+++ hajiboot-jpa/src/main/java/kazusato/hajibootjpa/HajibootJpaApplication.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 @SpringBootApplication
 public class HajibootJpaApplication implements CommandLineRunner {
@@ -17,7 +20,15 @@ public class HajibootJpaApplication implements CommandLineRunner {
     public void run(String... string) {
         Customer created = customerRepository.save(new Customer(null, "Hidetoshi", "Dekisugi"));
         System.out.println(created + " is created!");
-        customerRepository.findAllOrderByName().forEach(System.out::println);
+
+        Pageable pageable = new PageRequest(0, 3);
+        Page<Customer> page = customerRepository.findAll(pageable);
+
+        System.out.println("1ページのデータ数=" + page.getSize());
+        System.out.println("現在のページ=" + page.getNumber());
+        System.out.println("全ページ数=" + page.getTotalPages());
+        System.out.println("全データ数=" + page.getTotalElements());
+        page.getContent().forEach(System.out::println);
     }
 
     public static void main(String[] args) {
