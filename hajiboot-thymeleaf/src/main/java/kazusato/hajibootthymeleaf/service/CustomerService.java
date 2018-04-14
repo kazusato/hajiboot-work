41e85db252199ffea628276fded6b6bc35b94b14
Kazuhiko <sato@tent.sakura.ne.jp>
2018-04-14 23:08:03 +0900

    list.htmlの動的表示まで完了

---
+++ hajiboot-thymeleaf/src/main/java/kazusato/hajibootthymeleaf/service/CustomerService.java
@@ -0,0 +1,42 @@
+package kazusato.hajibootthymeleaf.service;
+
+import kazusato.hajibootthymeleaf.domain.Customer;
+import kazusato.hajibootthymeleaf.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomerService {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    public List<Customer> findAll() {
+        return customerRepository.findAll();
+    }
+
+    public Page<Customer> findAll(Pageable pageable) {
+        return customerRepository.findAll(pageable);
+    }
+
+    public Customer findOne(Integer id) {
+        return customerRepository.findOne(id);
+    }
+
+    public Customer create(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    public Customer update(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    public void delete(Integer id) {
+        customerRepository.delete(id);
+    }
+
+}
